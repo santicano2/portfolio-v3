e937e541e5b249357f4a36b45ec5a97d4e51cc93
Santi <canosantiago404@gmail.com>
2025-01-06 14:21:05 -0300

    feat: added hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <main className="px-10 sm:px-6">
+        <Hero />
+      </main>
     </>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,49 @@
+import { MotionDiv, MotionH1, MotionP } from "@/lib/motion";
+
+export const Hero = () => {
+  return (
+    <section id="home">
+      <MotionDiv className="mx-auto max-w-5xl">
+        <div className="flex h-screen flex-col items-center justify-center gap-3">
+          <MotionH1
+            initial={{ opacity: 0, x: 40 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true }}
+            transition={{
+              delay: 0.7,
+              duration: 0.3,
+            }}
+            className="text-center text-8xl font-bold mb-2 md:text-7xl sm:text-5xl"
+          >
+            Santiago Cano <span className="text-target">.</span>
+          </MotionH1>
+          <MotionP
+            initial={{ opacity: 0, x: -40 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true }}
+            transition={{
+              delay: 1,
+              duration: 0.3,
+            }}
+            className="text-center text-4xl sm:text-3xl"
+          >
+            Desarrollador{" "}
+            <span className="font-bold text-target">Full Stack</span>
+          </MotionP>
+          <MotionDiv
+            initial={{ opacity: 0, y: 40 }}
+            whileInView={{ opacity: 1.3, y: 0 }}
+            viewport={{ once: true }}
+            transition={{
+              delay: 1.2,
+              duration: 0.3,
+            }}
+            className="mt-4 text-target text-5xl"
+          >
+            ↓
+          </MotionDiv>
+        </div>
+      </MotionDiv>
+    </section>
+  );
+};
