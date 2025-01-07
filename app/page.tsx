3146423ffe2af386df04302f8b4545ce3810a94b
Santi <canosantiago404@gmail.com>
2025-01-06 23:37:44 -0300

    feat: added footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { About } from "@/components/About";
 import { Contact } from "@/components/Contact";
+import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { Projects } from "@/components/Projects";
@@ -18,6 +19,7 @@ export default function Home() {
         <Contact />
         <SocialLinks />
       </main>
+      <Footer />
     </>
   );
 }
