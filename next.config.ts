0569c947b8c343db8b9fb9c6bc380b1ac9bf445f
Santi <canosantiago404@gmail.com>
2025-01-07 14:52:31 -0300

    feat: github pages config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Github pages config
+  basePath: "/portfolio-v3",
+  output: "export",
+  reactStrictMode: true,
+
+  // Hostname config
   images: {
+    // Github pages only
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
