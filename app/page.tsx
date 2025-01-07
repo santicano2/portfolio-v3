5f8abcfbd1b7d359599fc2d5cd512505edaa064b
Santi <canosantiago404@gmail.com>
2025-01-06 23:34:58 -0300

    feat: added socialLinks component

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { Projects } from "@/components/Projects";
 import { Skills } from "@/components/Skills";
+import { SocialLinks } from "@/components/SocialLinks";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <Projects />
         <Skills />
         <Contact />
+        <SocialLinks />
       </main>
     </>
   );
---
+++ components/SocialLinks.tsx
@@ -0,0 +1,36 @@
+import { Github, Linkedin } from "lucide-react";
+
+import { MotionDiv } from "@/lib/motion";
+
+export const socialData = [
+  { link: "https://github.com/santicano2", icon: <Github />, title: "GitHub" },
+  {
+    link: "https://www.linkedin.com/in/santicano2/",
+    icon: <Linkedin />,
+    title: "Linkedin",
+  },
+];
+
+export const SocialLinks = () => {
+  return (
+    <MotionDiv
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
+      className="fixed bottom-0 left-10 flex flex-col items-center gap-y-4 md:hidden"
+    >
+      {socialData.map((social, index) => (
+        <a
+          key={index}
+          className="transition hover:-translate-y-1 hover:text-target"
+          target="_blank"
+          href={social.link}
+          rel="noreferrer"
+        >
+          {social.icon}
+        </a>
+      ))}
+      <div className="mt-4 h-32 w-[2px] origin-bottom bg-target"></div>
+    </MotionDiv>
+  );
+};
