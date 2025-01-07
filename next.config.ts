a61bad647d17dac9378ecab56c5d765b84dfe8c3
Santi <canosantiago404@gmail.com>
2025-01-07 15:12:07 -0300

    fix: config for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Github pages config
-  basePath: "/portfolio-v3",
-  output: "export",
-  reactStrictMode: true,
-
-  // Hostname config
   images: {
-    // Github pages only
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
